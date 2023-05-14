ac40c5288429937030e41daa87769642e3a808b3
André Nascimento <andre4.20nascimento@outlook.com>
2023-05-13 21:21:30 -0300

    Conclusao da Tarefa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿		double etanol , gasolina;
+
+        Console.WriteLine("--- Etanol ou Gasolina? ---\n");
+
+        Console.Write("Digite o preço do Etanol (R$).....: ");
+		etanol = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o preço do Gasolina (R$)...: ");
+        gasolina = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(Razao(etanol, gasolina));
+
+        if(Recomendação(etanol,gasolina))
+        {
+            Console.WriteLine("Recomendação: Abasteça com Gasolina.");
+        }
+        else
+        {
+            Console.WriteLine("Recomendação: Abasteça com Etanol.");
+        }
+    
+    static string Razao (double valor1, double valor2)
+    {
+        double valor3 = (valor1 / valor2) ;
+    
+        return $"\nO preço do etanol corresponde a {valor3:P} do preço da gasolina.\n";
+    }
+    
+	static bool Recomendação (double a , double g)
+    { 
+        bool gasolina = (
+     
+            (a/g)*100 >= 73 
+        );
+        return gasolina;
+    }
